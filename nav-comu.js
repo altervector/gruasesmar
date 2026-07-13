35ce839b8e94b44a26bf52094df1c77bcfa24821
genecarles <genecarles@gmail.com>
2026-07-13 10:57:51 +0200

    sghj

--- nav-comu.js
+++ nav-comu.js
@@ -21,7 +21,7 @@ class NavComu extends HTMLElement {
                     <li><a href="index.html#inici">${CONFIG.NAV_INI}</a></li>
                     <li><a href="index.html#qui-som">${CONFIG.NAV_NOS}</a></li>
                     <li><a href="index.html#serveis">${CONFIG.NAV_SER}</a></li>
-                    <li><a href="index.html#contacte">${CONFIG.NAV_CON}</a></li>
+                    <li><a href="#contacte">${CONFIG.NAV_CON}</a></li>
                 </ul>
             </nav>
         `;
