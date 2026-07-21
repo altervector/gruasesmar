265850f215c4b79da99e7b9f6086f8c197c6411a
genecarles <genecarles@gmail.com>
2026-07-21 19:18:46 +0200

    hsd

--- footer-comu.js
+++ footer-comu.js
@@ -70,7 +70,7 @@ class FooterComu extends HTMLElement {
                     <a href="${CONFIG.INSTAGRAM}" target="_blank">
                         <img src="${CONFIG.ASSETS}icon/Icoinsta.png" alt="Instagram" class="icona-app"> Instagram
                     </a>
-                    <a href="${CONFIG.URL_RESSENYES}}" target="_blank">
+                    <a href="${CONFIG.URL_RESSENYES}" target="_blank">
                         <img src="${CONFIG.ASSETS}icon/google.png" alt="Google" class="icona-app">Google
                     </a>
                 </div>
