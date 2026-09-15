d89e8008159b8b6c96cd1142eb3146e891f3d831
genecarles <genecarles@gmail.com>
2026-09-15 17:18:07 +0200

    ZFHD

--- footer-comu.js
+++ footer-comu.js
@@ -26,7 +26,7 @@ class FooterComu extends HTMLElement {
                 <h2 class="seccio-titol">${CONFIG.ON_SOM_TIT}</h2>
                 <div class="contacte-grid">
                     <div class="contacte-item">
-                        <div class="contacte-ico">${CONFIG.ARECA_ICO}</div>
+                        <div class="contacte-ico">${CONFIG.ADRECA_ICO}</div>
                         <div>
                             <div class="contacte-label">${CONFIG.ADRECA_LABEL}</div>
                             <div class="contacte-valor"><a href="${CONFIG.URL_MAPS}" target="_blank">${CONFIG.ADRECA}</a></div>
